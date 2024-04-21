4fcbcd60a14ce2dd0d6b393c436e8b5a037640f9
wjp <2435878928@qq.com>
2024-04-21 14:55:53 +0800

    feature: support zxcvbn to test password strength

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use clap::Parser;
 use rcli::{process_csv, process_genpass, Opts, SubCommand};
+use zxcvbn::zxcvbn;
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
         SubCommand::CSV(opt) => process_csv(opt)?,
         SubCommand::GenPass(opt) => {
-            let result = process_genpass(
+            let password = process_genpass(
                 opt.length,
                 opt.uppercase,
                 opt.lowercase,
                 opt.number,
                 opt.symbol,
-            );
-            println!("result: {:?}", result);
+            )?;
+            println!("password: {:?}", password);
+            let estimate = zxcvbn(&password, &[])?;
+            println!("Password strength: {:?}", estimate.score())
         }
     }
     Ok(())
